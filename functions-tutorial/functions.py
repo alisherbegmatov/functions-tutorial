280846e5551fed8e8ca6f37a3b2cb2ae92f00d41
alisherbegmatov <60242355+alisherbegmatov@users.noreply.github.com>
2020-09-07 02:03:34 -0700

    Add files via upload

---
+++ functions-tutorial/functions.py
@@ -0,0 +1,27 @@
+
+def area(width, height):
+    result = width * height
+    return result
+result1 = area(5, 6)
+result2 = area(7, 8)
+result3 =  area(9, 10)
+print(result1)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+result1 = subtract(1, 1)
+result2 = subtract(2, 2)
+result3 = subtract(3, 3)
+print(result1)
+print(result2)
+print(result3)
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+add1 = add(1, 1)
+add2 = add(2, 2)
+print(add1)
+print(add2)
